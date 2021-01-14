3e496b650ce5d965e446c1d8deff346b76940da6
coffeefolia <coffeefoliaforensics@gmail.com>
2021-01-14 13:45:58 -0500

    comment out some borrowed code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"debug/pe"
-	"encoding/binary"
 	"fmt"
 	"os"
 )
@@ -10,28 +9,29 @@ import (
 // variables
 var pfile pe.File
 var dataDir [16]pe.DataDirectory
-var sizeOptHeader32 = uint16(binary.Size(pe.OptionalHeader32{}))
-var sizeOptHeader64 = uint16(binary.Size(pe.OptionalHeader64{}))
+
+// var sizeOptHeader32 = uint16(binary.Size(pe.OptionalHeader32{}))
+// var sizeOptHeader64 = uint16(binary.Size(pe.OptionalHeader64{}))
 
 // GET - PE Information Functions
 // Thanks to https://gist.github.com/nokute78/46c1eb6a2f6050db4c5a87845dbdf87c
-func getOptionalHeader(f *pe.File) {
-	fmt.Print("Optional Header Size: ")
-	fmt.Println(sizeOptHeader32, " ", sizeOptHeader64)
-	switch f.OptionalHeader.(type) {
-	case *pe.OptionalHeader32:
-		fmt.Fprintf(os.Stdout, "Header32\n")
-		o := f.OptionalHeader.(*pe.OptionalHeader32)
-		dataDir = o.DataDirectory
-	case *pe.OptionalHeader64:
-		fmt.Fprintf(os.Stdout, "Header64\n")
-		o := f.OptionalHeader.(*pe.OptionalHeader64)
-		dataDir = o.DataDirectory
-	default:
-		fmt.Fprintf(os.Stderr, "INVALID FORMAT\n")
-		os.Exit(1)
-	}
-}
+// func getOptionalHeader(f *pe.File) {
+// 	fmt.Print("Optional Header Size: ")
+// 	fmt.Println(sizeOptHeader32, " ", sizeOptHeader64)
+// 	switch f.OptionalHeader.(type) {
+// 	case *pe.OptionalHeader32:
+// 		fmt.Fprintf(os.Stdout, "Header32\n")
+// 		o := f.OptionalHeader.(*pe.OptionalHeader32)
+// 		dataDir = o.DataDirectory
+// 	case *pe.OptionalHeader64:
+// 		fmt.Fprintf(os.Stdout, "Header64\n")
+// 		o := f.OptionalHeader.(*pe.OptionalHeader64)
+// 		dataDir = o.DataDirectory
+// 	default:
+// 		fmt.Fprintf(os.Stderr, "INVALID FORMAT\n")
+// 		os.Exit(1)
+// 	}
+// }
 
 func getSections(f *pe.File) {
 	fmt.Println("Sections: ")
